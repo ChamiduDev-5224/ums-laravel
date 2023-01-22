8ae095fd83b26f27e7ed4c7725954f784495aacb
Chamidu-circlebook <72206025+ChamiduDev-5224@users.noreply.github.com>
2023-01-22 13:07:42 +0530

    bootstrap setup

---
+++ ums/resources/views/users/register.blade.php
@@ -0,0 +1,8 @@
+@extends('users.layout')
+
+@section('content')
+
+<h1>helow world</h1>
+<button><a href="/users">regisiter</a></button>
+
+@endsection
--- ums/routes/web.php
+++ ums/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('users.register');
 });
+
+Route::get('users', function () {
+return view('users.layout');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
